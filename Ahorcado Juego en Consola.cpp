e5d59ac15763274e7a6d7e3d1d28f5480a9b8f16
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-07-10 14:41:31 -0600

    Initial commit

---
+++ Ahorcado Juego en Consola.cpp
@@ -0,0 +1,20 @@
+/*Ahorcado Juego en Consola C++ */
+
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+#include<Windows.h>
+
+std::string palabras[2] = {"Juan","Discord"};
+
+
+
+int main()
+{
+	system("CLS");
+
+	std::cout << palabras[1];
+
+	system("pause");
+	return 0;
+}
